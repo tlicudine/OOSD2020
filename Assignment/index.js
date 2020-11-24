0493cef4e318cf47fff4e30d38e2424cb3fa9059
tlicudine <t.licudine@gmail.com>
2020-11-23 23:11:13 -0700

    Moving directories again and complete Assignment 7

---
+++ Assignment/index.js
@@ -0,0 +1,27 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("topnav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  	if (window.pageYOffset >= sticky) {
+    	navbar.classList.add("sticky")
+  	} else {
+    	navbar.classList.remove("sticky");
+	}
+}
+
+function gallery() {
+	for getElementById() in somewhere {
+
+	}
+
+function imgExpand(x) {
+  x.style.height = "64px";
+  x.style.width = "64px";
+}
+
+function imgReturn(x) {
+  x.style.height = "32px";
+  x.style.width = "32px";
+}
