7b7fc960d33337de3cf17afdb514eb0e603aba6d
tlicudine <t.licudine@gmail.com>
2021-01-12 08:15:43 -0700

    Reorganization

--- Assignment/js/register.js
+++ Assignment/js/register.js
@@ -1,16 +1,10 @@
-function inputhover(x){
-	
-}
-
 function validate(myForm) {
-	var fName = document.getElementById("fName")
+	var form = document.getElementById(".form-container")
 
-	for  in document.form() {
-		if el=="" {
-			alert(el +"must have a value");
+	for (el in document.form()) {
+		if (el == "") {
+			alert(el + "must have a value");
 			return false
-		}
-	})
+		};
+	};
 }
-
-onmouseover = ()
--- sait-node/sait-express/index.js
+++ sait-node/sait-express/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const assert = require('assert');
-const app = express();
+
 const bodyParser = require('body-parser');
 const greeting = require("./greeting.js");
 
@@ -10,16 +11,21 @@ app.set('view engine', 'pug');
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'views')));
 
 app.listen(8000, function () {
     console.log('Server started on port 8000');
 });
 
-app.get('/', (req, res) => {
+app.get('/' || "/index", (req, res) => {
     let greet = greeting.randomGreeting();
-    res.render('index', {greet});
+    res.render('index', { greet });
+});
+
+app.get('/index', (req, res) => {
+    let greet = greeting.randomGreeting();
+    res.render('index', { greet });
 });
 
 app.get('/contact', (req, res) => {
@@ -32,4 +38,35 @@ app.get('/about', (req, res) => {
 
 app.get('*', (req, res) => {
     res.render('404');
+});
+
+const MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://127.0.0.1:27017'
+const dbName = "database"
+let db
+
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+    if (err) return console.log(err);
+
+    db = client.db('database');
+    const contactCollection = db.collection('contacts');
+    console.log(`Connected MongoDB: ${url}`)
+    console.log(`Database: ${dbName}`)
+
+    app.post('/contactform', (req, res) => {
+        contactCollection.insertOne(req.body)
+            .then(res => {
+                console.log(res)
+            })
+            .catch(error => console.error(error))
+        console.log(res);
+        let contactName = contactCollection.find().toArray();
+        console.log(contactName);
+        res.render("thankyou", { contactName })
+    });
+
+    app.get('/', (req, res) => {
+        const cursor = db.collection('quotes').find();
+
+    })
 });
